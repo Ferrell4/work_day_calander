7731e6e0cf26a87ce70717f529c7149d0ca10c4e
Ferrell4 <32971621+Ferrell4@users.noreply.github.com>
2022-09-21 20:24:12 -0400

    fixing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
 $('#currentDay').html(moment().format('LLLL'));
 
-function timeBlock() {
-    var hour = moment().hours();
-    console.log(hour);
-
-    //loop to color code the times
-    $(".block").each(function () {
-        var currentHr = parseInt($(this).attr("id"));
-        console.log(currentHr);
-
-        
-        if (currentHr < hour) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        } else if (currentHr == hour) {
-            $(this).removeClass("past");
-            $(this).removeClass("future");
-            $(this).addClass("present");
-        } else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-        }
-        console.log(this);
+$(document).ready(function() { 
+    function timeBlock() {
+        var hour = moment().hours();
+        console.log(hour);
     
-    })
-};
+        $(".block").each(function () {
+            var currentHr = parseInt($(this).attr("id"));
+            console.log(currentHr);
+    
+            if (currentHr < hour) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            } else if (currentHr == hour) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+            console.log(this);
+        
+        })
+    };
+})
